828cbe11b209666462cb621f4174bc66c3f3a924
Jesse Chase <jchase6037@gmail.com>
2022-06-26 12:55:23 -0500

    Added Create Client section and functions for btns

--- js/oteam.js
+++ js/oteam.js
@@ -2,6 +2,7 @@ var loginSection = document.getElementById("login-section");
 var profileSection = document.getElementById("profile-section");
 var logOutButton = document.getElementById("log-out-button")
 var navItems = document.getElementById("nav-list-item");
+var addClientSection = document.getElementById("add-client-section");
 
 function LogOut() {
     profileSection.classList.add("hidden");
@@ -12,7 +13,17 @@ function LogOut() {
 
 function LogIn() {
     loginSection.classList.add("hidden");
-    profileSection.classList.remove("hidden")
+    profileSection.classList.remove("hidden");
     logOutButton.classList.remove("hidden");
     navItems.classList.remove("hidden");
+}
+
+function ShowAddClientSection() {
+    addClientSection.classList.remove("hidden");
+    profileSection.classList.add("hidden");
+}
+
+function SubmitAdd() {
+    addClientSection.classList.add("hidden");
+    profileSection.classList.remove("hidden");
 }
