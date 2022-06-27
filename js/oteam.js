6108e2ca2c5ea3361ed436a8bd12cef821d522f4
Jesse Chase <jchase6037@gmail.com>
2022-06-26 19:06:57 -0500

    Added interventions page under resources

--- js/oteam.js
+++ js/oteam.js
@@ -3,12 +3,16 @@ var profileSection = document.getElementById("profile-section");
 var logOutButton = document.getElementById("log-out-button")
 var navItems = document.getElementById("nav-list-item");
 var addClientSection = document.getElementById("add-client-section");
+var interventionSection = document.getElementById("interventions-section");
+var homeNavLink = document.getElementById("home-nav-link")
+var resourcesNavLink = document.getElementById("resources-nav-link")
 
 function LogOut() {
     profileSection.classList.add("hidden");
     loginSection.classList.remove("hidden");
     logOutButton.classList.add("hidden");
     navItems.classList.add("hidden");
+    homeNavLink.classList.remove("active");
 }
 
 function LogIn() {
@@ -16,6 +20,8 @@ function LogIn() {
     profileSection.classList.remove("hidden");
     logOutButton.classList.remove("hidden");
     navItems.classList.remove("hidden");
+    homeNavLink.classList.add("active");
+    
 }
 
 function ShowAddClientSection() {
@@ -26,4 +32,20 @@ function ShowAddClientSection() {
 function SubmitAdd() {
     addClientSection.classList.add("hidden");
     profileSection.classList.remove("hidden");
+}
+
+function ShowInterventions() {
+    profileSection.classList.add("hidden");
+    interventionSection.classList.remove("hidden");
+    homeNavLink.classList.remove("active");
+    resourcesNavLink.classList.add("active");
+    addClientSection.classList.add("hidden");
+}
+
+function ShowHome() {
+    resourcesNavLink.classList.remove("active");
+    homeNavLink.classList.add("active");
+    interventionSection.classList.add("hidden");
+    profileSection.classList.remove("hidden");
+    addClientSection.classList.add("hidden");
 }
